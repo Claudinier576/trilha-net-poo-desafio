3fffab77a5669f73aaa074c4ae9b1104efe70e8b
Claudinier Neto <devneto576@gmail.com>
2023-08-21 10:02:59 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new(numero: "21999999999", modelo: "Nokia teste", imei: "222222", memoria: 128);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+
+Console.WriteLine("\n");
+
+Iphone iphone = new(numero: "21999999999", modelo: "Nokia teste", imei: "222222", memoria: 128);
+
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
